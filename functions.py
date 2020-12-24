3fae78883d93259afac03d9f405feb63c77c7ac7
Caroline Eliasson <76491539+CAEL01@users.noreply.github.com>
2020-12-24 20:46:34 +0100

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,43 @@
+# Student: Caroline Eliasson, eliasson.caroline@gmail.com
+
+"""
+Extra credit:
+One of the data types we haven't covered yet is called "booleans". 
+When a variable is set to True or False, that's a boolean. For 
+extra credit, see if you can figure out how to use booleans, and 
+add an extra function that returns a boolean value instead of a 
+String or Number. Hint: make sure to capitalize the first letter 
+in the words True or False when you use them. We'll cover booleans 
+more in the lecture on "if" statements coming up next in the course.
+"""
+
+    # Functions
+
+def Genre():
+    print('Pop')
+Genre()
+
+def Artist():
+    print('Andy Grammer')
+Artist()
+
+def Song():
+    print('Song: I Found You')
+Song()
+
+def Year():
+    print('Release year: 2019')
+Year()
+
+def myYear(year):
+    print('Current year: '+year)
+myYear("2020")
+
+
+# Booleans (for extra credit).
+
+# This has a String value. I am just experimenting here. :)
+if 2019:
+    print('Label: BigBeats Records')
+
+# This function generates a boolean value of True or False. A boolean can only take two values: True or False.
