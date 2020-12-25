482b792357c561cbf38bd292781ddc5a3c0f3532
Caroline Eliasson <eliasson.caroline@gmail.com>
2020-12-25 18:15:48 +0100

    Deleted unecessary blank lines.

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 # Student: Caroline Eliasson
 
 # Functions
-
 def Genre():
     print('Pop')
 Genre()
@@ -22,17 +21,14 @@ def myYear(year):
     print('Current year: '+year)
 myYear("2020")
 
-
 # Booleans (for extra credit)
 # This has a String value stating that "if" the release year is 2019, write "Label: Big Beats Records".
 if 2019:
     print('Label: BigBeats Records')
 
-
 """
 This generates a 2 boolean values, one True and on False. I have created a boolean that simply verifies that 3*3+3=12 (True) and that there is no letter "z" in the name
 Andy Grammer (False).
 """
-
 print((3*3)+3==12)
 print("z" in "Andy Grammer")
