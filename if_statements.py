75db5d122862b72c9af4ab14e4ebbc0f06d6b4b9
Caroline Eliasson <eliasson.caroline@gmail.com>
2020-12-27 00:13:17 +0100

    Update if_statements.py

--- if_statements.py
+++ if_statements.py
@@ -1,9 +1,18 @@
-    #Function with three parameters using equality 
+    #Function with three parameters using equality. 
 def function(para1,para2,para3):
+  para1=int(input("enter any number"))
+  para2=int(input("enter any number"))
+  para3=int(input("enter any number"))
   if para1==para2:
     return True
   if para1==para3:
-      return True
+    return True
   if para2==para3:
-      return True
+    return True
   return False
+
+Para1=0
+Para2=0
+Para3=0
+
+function("para1", "para2", "para3")
