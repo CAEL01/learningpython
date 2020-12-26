4127cbabc46d9b2f186c16f953f34e7db8f5b095
Caroline Eliasson <eliasson.caroline@gmail.com>
2020-12-26 13:56:56 +0100

    Changed the code completely.

--- if_statements.py
+++ if_statements.py
@@ -1,14 +1,22 @@
-#When a specific condition occurs, a specific action takes place.
+"""
+Here I'm taking my pajamas on or off depending on the time of day and/or
+if I'm sleepy or not.
+"""
+#Note to Self: When a specific condition occurs, a specific action takes place.
 
-#if condition:
-#   Action: 
+Time  = "Day"
+Sleepy = False
+Pajamas = "Unknown"
+InBed = True
+print(Pajamas)
 
-click = False
-Like = 0
-click = True
+# Function that checks if it's night time, if we're sleepy - then we put on pajamas.
+if Time == "Night":
+    Pajamas = "On"
 
-if click == True:
-    Like = Like + 1
-    click = False
+elif Time == "Morning": #"elif" is short for "else if"
+    Pajamas = "On"
 
-print(Like)
+else:
+    Pajamas = "Off"
+    print(Pajamas)
