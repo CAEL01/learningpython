24b857febb71cdddc15190b8834dd94ff633adb2
Caroline Eliasson <eliasson.caroline@gmail.com>
2020-12-26 13:16:28 +0100

    Added code

--- if_statements.py
+++ if_statements.py
@@ -1,1 +1,16 @@
+#When a specific condition occurs, a specific action takes place.
 
+#if condition:
+#   Action: 
+
+click = False
+
+Like = 0
+
+click = True
+
+if click == True:
+    Like = Like + 1
+    click = False
+
+print(Like)
