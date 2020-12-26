714529a4cf1003bd8c824083ff16207a719a8381
Caroline Eliasson <eliasson.caroline@gmail.com>
2020-12-26 22:45:20 +0100

    Update if_statements.py

--- if_statements.py
+++ if_statements.py
@@ -1,12 +1,9 @@
     #Function with three parameters using equality 
 def function(para1,para2,para3):
   if para1==para2:
-    print("True")
     return True
   if para1==para3:
-      print("True")
       return True
   if para2==para3:
-      print("True")
       return True
   return False
